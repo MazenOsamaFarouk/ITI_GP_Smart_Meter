6087e97aeadf6319bfacb560df8b6a6112e24210
Mazen Osama <mazen.osama.017@gmail.com>
2021-03-13 20:51:06 +0000

    orgainzed code into threads and added power and energy calculations

--- Rpi-Python_SourceCode/UART_with_threading_test.py
+++ Rpi-Python_SourceCode/UART_with_threading_test.py
@@ -4,14 +4,15 @@ from threading import Timer,Thread, Lock
 
 def PrintQueue():
     global uart_q
-    print(uart_q.get())
-    timer1 = Timer(5,PrintQueue)
+    # print(list(uart_q.queue))
+    timer1 = Timer(1,PrintQueue)
     timer1.start()
 
 
 def FillQueue(the_q, the_lock):
     while True:
         rx=UART_Getline()
+        print(rx)
         with the_lock:
             the_q.put(rx)
         # the_q.task_done()
@@ -20,7 +21,7 @@ UART_Init()
 
 lock = Lock()
 uart_q = Queue()
-timer1 = Timer(5,PrintQueue)
+timer1 = Timer(1,PrintQueue)
 uart_thread = Thread(target=FillQueue, args=(uart_q,lock)) 
 uart_thread.daemon = True
 
